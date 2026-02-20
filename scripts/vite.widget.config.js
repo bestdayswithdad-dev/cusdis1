a317df361ff2a60555d513bbac4c13ad4de6ae19
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-02-21 10:20:19 +1030

    Update vite.widget.config.js

--- scripts/vite.widget.config.js
+++ scripts/vite.widget.config.js
@@ -6,7 +6,7 @@ import svelte from 'rollup-plugin-svelte';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
+const config = {
   root: 'widget',
   build: {
     lib: {
@@ -23,4 +23,6 @@ export default defineConfig({
       emitCss: false,
     }),
   ],
-});
+};
+
+export default defineConfig(() => config);
