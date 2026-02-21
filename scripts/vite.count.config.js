77bf041f1cd33d28f2d5119494e59ab95aa9447d
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-02-21 10:48:42 +1030

    Update vite.count.config.js

--- scripts/vite.count.config.js
+++ scripts/vite.count.config.js
@@ -1,8 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import svelte from 'rollup-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// This replaces the old __dirname that crashed Node 20
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
