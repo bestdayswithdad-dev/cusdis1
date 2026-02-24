d1312f9cbb5c38b7d6a990c6f7de6b36391cfee2
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-02-24 21:48:59 +1030

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './widget/**/*.svelte',
+    './widget/**/*.html',
+    './widget/theme.css'
+  ],
+  darkMode: 'class', 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
