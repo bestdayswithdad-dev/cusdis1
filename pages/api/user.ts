6091beafe96e21f1f5fd24f99b0b94a69d1d6fff
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-02-25 18:45:17 +1030

    Update user.ts

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,29 +1,31 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { AuthService } from "../../service/auth.service";
 import { UserService } from "../../service/user.service";
+import { getSession } from "../../utils.server";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
+  // 1. Get the session to find the 'user'
+  const session = await getSession(req);
+  if (!session) {
+    return res.status(401).send("Unauthorized");
+  }
 
-  const userService = new UserService(req)
-  const authService = new AuthService(req, res)
+  const userService = new UserService(req);
 
-  if (req.method === 'PUT') {
-   // 1. Update the destructuring at the top of the handler
-const { 
-  displayName, 
-  notificationEmail, 
-  enableCommentNotifications // RENAME THIS from enableNewCommentNotification
-} = req.body
+  // 2. Correctly destructure the renamed field
+  const { 
+    displayName, 
+    notificationEmail, 
+    enableCommentNotifications 
+  } = req.body;
 
-// ... some code ...
+  // 3. Use 'session.uid' instead of the missing 'user.uid'
+  await userService.update(session.uid, {
+    enableCommentNotifications,
+    notificationEmail,
+    displayName
+  });
 
-// 2. Update the call to the user service (Line 28)
-await userService.update(user.uid, {
-  enableCommentNotifications, // RENAME THIS from enableNewCommentNotification
-  notificationEmail,
-  displayName
-})
-
+  return res.status(200).json({ success: true });
+}
     res.json({
       message: 'success'
     })
