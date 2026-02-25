5319e55cceabd4be42590679d6123f7c896531f7
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-02-25 18:33:06 +1030

    Update notification.service.ts

--- service/notification.service.ts
+++ service/notification.service.ts
@@ -30,7 +30,8 @@ export class NotificationService extends RequestScopeService {
           select: {
             id: true,
             email: true,
-            enableNewCommentNotification: true,
+            // CHANGED: matched with new schema name
+            enableCommentNotifications: true,
             notificationEmail: true,
           },
         },
@@ -64,7 +65,8 @@ export class NotificationService extends RequestScopeService {
     const notificationEmail =
       project.owner.notificationEmail || project.owner.email
 
-    if (project.owner.enableNewCommentNotification) {
+    // CHANGED: matched with new schema name
+    if (project.owner.enableCommentNotifications) {
       let unsubscribeToken = this.tokenService.genUnsubscribeNewCommentToken(
         project.owner.id,
       )
