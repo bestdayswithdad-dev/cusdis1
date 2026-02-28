113253d96d2e79ceabb2f7846dbf57a51016b079
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-03-01 00:17:59 +1030

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
 export default {
-  rewrites() {
+  async rewrites() {
     return [
       {
         source: '/doc',
         destination: '/doc/index.html'
       }
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Target your script files to prevent ORB blocking
+        source: "/:path*.(js|mjs)",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/javascript",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allows your blog to fetch the script from this Vercel domain
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin", // Explicitly tells ORB this is a shared resource
+          }
+        ],
+      },
+    ];
+  },
 }
