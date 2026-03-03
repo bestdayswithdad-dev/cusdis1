835ab47e8e19edeba6ef35992f1b6af1ad36b79e
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-03-03 21:26:01 +1030

    Update [commentId].ts

--- pages/api/comment/[commentId].ts
+++ pages/api/comment/[commentId].ts
@@ -1,12 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { CommentService } from '../../../service/comment.service'
+import { getSession } from '../../../utils.server'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // 1. Verify the user is actually logged in via Supabase
+  const session = await getSession(req, res)
+  
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
   const commentService = new CommentService(req)
+  
   if (req.method === 'DELETE') {
-   await commentService.deleteComment(req.query.commentId as string)
-    res.json({
-      message: 'Success'
-    })
+    // 2. The service should now be able to use session.uid to verify ownership
+    await commentService.deleteComment(req.query.commentId as string)
+    res.json({ message: 'Success' })
   }
 }
