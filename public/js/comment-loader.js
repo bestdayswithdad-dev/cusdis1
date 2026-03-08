10c3364081e3644a7a9f3567d7bc4a3aeda4a709
bestdayswithdad-dev <bestdayswithdad@gmail.com>
2026-03-08 20:59:14 +1030

    Update comment-loader.js

--- public/js/comment-loader.js
+++ public/js/comment-loader.js
@@ -26,7 +26,7 @@
         }
         
         .comment-disclaimer {
-            margin-top: -30px !important;
+            margin-top: -40px !important;
             backdrop-filter: blur(10px);
             -webkit-backdrop-filter: blur(10px);
             padding: 12px;
